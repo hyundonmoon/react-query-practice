d99aa185569c828983838e6ee94fe92ccca61953
Hyun Don Moon <hyundonmoon@gmail.com>
2025-01-11 14:06:42 +0900

    update query

--- src/hooks/useHackerNewStories.ts
+++ src/hooks/useHackerNewStories.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, queryOptions, useQuery } from "@tanstack/react-query";
 import { HNStoryApiResponseSchema } from "../schema";
 
 async function getHackerNewsStories(page: number) {
@@ -13,12 +13,29 @@ async function getHackerNewsStories(page: number) {
   return response.json();
 }
 
-export function useHackerNewsStories(page: number) {
-  return useQuery({
+function getHackerNewsQueryOptions(page: number) {
+  return queryOptions({
     queryKey: ["stories", { page }],
     queryFn: async () => {
       const result = await getHackerNewsStories(page);
       return HNStoryApiResponseSchema.parse(result);
     },
+    staleTime: 1000 * 60 * 10,
+  });
+}
+
+export function prefetchHackerNewsStories(
+  queryClient: QueryClient,
+  page: number
+) {
+  return queryClient.prefetchQuery(getHackerNewsQueryOptions(page));
+}
+
+export function useHackerNewsStories(page: number) {
+  return useQuery({
+    ...getHackerNewsQueryOptions(page),
+    placeholderData: (prev) => prev,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
   });
 }
